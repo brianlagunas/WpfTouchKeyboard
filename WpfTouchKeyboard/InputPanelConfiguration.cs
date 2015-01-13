abe1d8c574517c5feb5d577c6508ecc792981a29
Brian Lagunas <brianlagunas@users.noreply.github.com>
2015-01-13 10:10:34 -0700

    initial add to source control

---
+++ WpfTouchKeyboard/InputPanelConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WpfTouchKeyboard
+{
+    [Guid("41C81592-514C-48BD-A22E-E6AF638521A6")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IInputPanelConfiguration
+    {
+        /// <summary>
+        /// Enables a client process to opt-in to the focus tracking mechanism for Windows Store apps that controls the invoking and dismissing semantics of the touch keyboard.
+        /// </summary>
+        /// <returns>If this method succeeds, it returns S_OK. Otherwise, it returns an HRESULT error code.</returns>
+        int EnableFocusTracking();
+    }
+
+    [ComImport, Guid("2853ADD3-F096-4C63-A78F-7FA3EA837FB7")]
+    class InputPanelConfiguration
+    {
+    }
+}
---
+++ WpfTouchKeyboard/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace WpfTouchKeyboard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+
+            // Disables inking in the WPF application and enables us to track touch events to properly trigger the touch keyboard
+            InkInputHelper.DisableWPFTabletSupport();
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Windows 8 API to enable touch keyboard to monitor for focus tracking in this WPF application
+            InputPanelConfiguration cp = new InputPanelConfiguration();
+            IInputPanelConfiguration icp = cp as IInputPanelConfiguration;
+            if (icp != null)
+                icp.EnableFocusTracking();
+        }
+    }
+}
